38b525a9bbbe85c48f54699106cc6731a436d763
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-07-07 15:58:48 +0900

    design: 헤더 디자인 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,21 @@
 import React from "react";
+import "../components/Header.css";
+import CartImg from "../assets/cart.svg";
 
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <div className="header">
+      <a href="/" className="header-logo">
+        Delivery Order
+      </a>
+      <div className="header-cart">
+        장바구니
+        <div className="header-cart-img">
+          <img src={CartImg} alt="cart" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
